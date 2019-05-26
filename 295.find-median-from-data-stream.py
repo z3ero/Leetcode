212033530b899abffaf237282585b1b558dd1577
z3ero <lzzmyth@gmail.com>
2019-05-26 23:36:26 +0800

    4

---
+++ 295.find-median-from-data-stream.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=295 lang=python3
+#
+# [295] Find Median from Data Stream
+#
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        
+
+    def addNum(self, num: int) -> None:
+        
+
+    def findMedian(self) -> float:
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
